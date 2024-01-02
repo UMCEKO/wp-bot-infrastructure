3d7a5520b0e94b959b639c52a9639c3a917745b8
Berkay Çakıbey <cakibey368@gmail.com>
2024-01-03 02:20:36 +0300

    init commit

--- src/commands/example.ts
+++ src/commands/example.ts
@@ -1,21 +1,3 @@
-// import {Message} from "whatsapp-web.js"
-// import {client} from "../index";
-// import * as fs from "fs";
-// import * as path from "path";
-//
-// module.exports = {
-//   name: "test",
-//   execute: async (message: Message, args: Array<string>)=>{
-//     if (!message.hasQuotedMsg) return
-//     let targetMessage = await message.getQuotedMessage()
-//     let contacts = JSON.stringify(targetMessage.vCards, null, 2)
-//     let contactsUnparsedArr: string[] = contacts.match(/waid=[0-9]*/g)
-//     let contactsParsedArr = contactsUnparsedArr.map(value => value.replaceAll("waid=", "") + "@c.us")
-//     fs.writeFileSync(path.join(__dirname, "../../group1.json"), JSON.stringify(contactsParsedArr, null, 2))
-//   }
-// }
-//
-
 import { Message } from "whatsapp-web.js";
 import {client} from "../index";
 import * as fs from "fs";
